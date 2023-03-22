8cc477bff544699e42330dc934372d2f3cde36ce
agawas <agawas@iu.edu>
2023-03-21 20:09:06 -0400

    Second Commit

--- customerservice/src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
+++ customerservice/src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
@@ -2,6 +2,7 @@ package edu.iu.p565.customerservice.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import edu.iu.p565.customerservice.model.Customer;
 @RequestMapping("/customers")
 public class CustomerController {
 
+    @GetMapping
     public List<Customer> findAll (){
         return null;
     }
